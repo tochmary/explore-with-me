1d5fcf1eb0ba459859b534253cb7a19ffa4c50be
tochmary <maria.tochka@yandex.ru>
2022-09-20 22:03:50 +0300

    sprint18_feat: эндпойнты для categories 2

---
+++ main-service/src/main/java/ru/practicum/mainservice/category/controller/CategoryAdminController.java
@@ -0,0 +1,45 @@
+package ru.practicum.mainservice.category.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.mainservice.category.mapper.CategoryMapper;
+import ru.practicum.mainservice.category.model.dto.CategoryDto;
+import ru.practicum.mainservice.category.model.entity.Category;
+import ru.practicum.mainservice.category.service.CategoryService;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/categories")
+public class CategoryAdminController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public CategoryDto addCategory(@RequestBody CategoryDto categoryDto) {
+        log.info("Добавление категории {}", categoryDto);
+        Category category = CategoryMapper.toCategory(categoryDto);
+        category = categoryService.addCategory(category);
+        return CategoryMapper.toCategoryDto(category);
+    }
+
+    @PatchMapping
+    public CategoryDto updateCategory(@RequestBody CategoryDto categoryDto) {
+        log.info("Изменение категории {}", categoryDto);
+        Category category = CategoryMapper.toCategory(categoryDto);
+        category = categoryService.updateCategory(category);
+        return CategoryMapper.toCategoryDto(category);
+    }
+
+    @DeleteMapping("/{catId}")
+    public void deleteCategory(@PathVariable long catId) {
+        log.info("Удаление категории с catId={}", catId);
+        categoryService.deleteCategory(catId);
+    }
+}
--- main-service/src/main/java/ru/practicum/mainservice/category/controller/CategoryController.java
+++ main-service/src/main/java/ru/practicum/mainservice/category/controller/CategoryController.java
@@ -2,55 +2,37 @@ package ru.practicum.mainservice.category.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.mainservice.category.mapper.CategoryMapper;
 import ru.practicum.mainservice.category.model.dto.CategoryDto;
 import ru.practicum.mainservice.category.model.entity.Category;
 import ru.practicum.mainservice.category.service.CategoryService;
 
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @GetMapping("/categories")
-    public List<CategoryDto> getCategories(@RequestParam(defaultValue = "0") Integer from,
-                                           @RequestParam(defaultValue = "20") Integer size) {
+    @GetMapping
+    public List<CategoryDto> getCategories(@PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
+                                           @Positive @RequestParam(defaultValue = "20") Integer size) {
         log.info("Получение списка категорий для from={}, size={}", from, size);
         List<Category> categoryList = categoryService.getCategories(from, size);
         return CategoryMapper.getCategoryDtoList(categoryList);
     }
 
-    @GetMapping("/categories/{catId}")
+    @GetMapping("/{catId}")
     public CategoryDto getCategoryById(@PathVariable long catId) {
         log.info("Получение категории с catId={}", catId);
         Category category = categoryService.getCategoryById(catId);
         return CategoryMapper.toCategoryDto(category);
-    }
-
-    @PostMapping("/admin/categories")
-    public CategoryDto addCategory(@RequestBody CategoryDto categoryDto) {
-        log.info("Добавление категории {}", categoryDto);
-        Category category = CategoryMapper.toCategory(categoryDto);
-        category = categoryService.addCategory(category);
-        return CategoryMapper.toCategoryDto(category);
-    }
-
-    @PatchMapping("/admin/categories")
-    public CategoryDto updateCategory(@RequestBody CategoryDto categoryDto) {
-        log.info("Добавление категории {} с catId={}", categoryDto);
-        Category category = CategoryMapper.toCategory(categoryDto);
-        category = categoryService.updateCategory(category);
-        return CategoryMapper.toCategoryDto(category);
-    }
-
-    @DeleteMapping("/admin/categories/{catId}")
-    public void deleteCategory(@PathVariable long catId) {
-        log.info("Удаление категории с catId={}", catId);
-        categoryService.deleteCategory(catId);
     }
 }
